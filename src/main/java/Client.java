ce9cdbee5e5750dab73cdb9ea919c111a9454e63
Allen Jue <mrallenjue@yahoo.com>
2022-12-17 00:53:12 -0600

    Created documentation for Client.java

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,7 +1,15 @@
+/**
+ * Author: Allen Jue
+ * Date: 12/17/2022
+ */
+
 import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
+/**
+ * An instance of a client that has the ability to connect to a server given an IP address and a port.
+ */
 public class Client {
     private Socket clientSocket;           /* Socket on the client side */
     private PrintWriter clientOutput;      /* Synchronous writer from the client */
@@ -11,23 +19,46 @@ public class Client {
      * Establishes a connection for a client
      * @param ip_addr target IP address for a client connection
      * @param port number to identify a process
-     * @throws IOException error when trying to establish a connection
      */
-    public void startConnection(String ip_addr, int port) throws IOException {
-        clientSocket = new Socket(ip_addr, port);
-        clientOutput = new PrintWriter(clientSocket.getOutputStream(), true);
-        clientInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    public void startConnection(String ip_addr, int port)  {
+        try {
+            clientSocket = new Socket(ip_addr, port);
+            clientOutput = new PrintWriter(clientSocket.getOutputStream(), true);
+            clientInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (Exception e) {
+            System.out.println("Network error");
+        }
     }
 
-    public String sendMessage(String message) throws IOException {
+    /**
+     * Sends a string to the server and blocks until the server repsonds with confirmation
+     * @param message to be sent to server
+     * @return "Message received: <message>" if the message was valid
+     *         "Exiting" if the message was 'quit'
+     *         null if there was a network failure
+     */
+    public String sendMessage(String message) {
         clientOutput.println(message);
-        String resp = clientInput.readLine();
-        return resp;
+        try {
+            String resp = clientInput.readLine();
+            return resp;
+        } catch (Exception e) {
+            System.out.println("Network error: Could not read input");
+            return null;
+        }
     }
 
-    public void stopConnection() throws IOException {
-        clientInput.close();
-        clientOutput.close();
-        clientSocket.close();
+    /**
+     * Closes a client connection.
+     */
+    public void stopConnection() {
+        try {
+            clientInput.close();
+            clientOutput.close();
+            clientSocket.close();
+        } catch (Exception e) {
+            System.out.println("Failed to close connection");
+        }
+
     }
 }
