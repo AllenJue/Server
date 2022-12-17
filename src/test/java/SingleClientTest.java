d4d019d87acf3a2ccde49bf8a2d9013d8264f993
Allen Jue <mrallenjue@yahoo.com>
2022-12-17 01:08:03 -0600

    Documented SingleClientTest

--- src/test/java/SingleClientTest.java
+++ src/test/java/SingleClientTest.java
@@ -1,11 +1,22 @@
+/**
+ * Author: Allen Jue
+ * Data: 12/17/2022
+ */
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Tests for accuracy within a single client-server relationship.
+ * Note that test cases will pass if the network is simply not working.
+ */
 public class SingleClientTest {
-    private static final String FREUD = "128.83.120.232";
-    private static final int TEST_PORT = 3000;
+    private static final String FREUD = "128.83.120.232";   /* Default IP Address, lab machine */
+    private static final int TEST_PORT = 3000;              /* Arbitrary port used */
 
+    /**
+     * Tests that a single client can send and close messages
+     */
     @Test
     public void test_msg_correct_single_client() {
         Client client = new Client();
@@ -22,6 +33,9 @@ public class SingleClientTest {
         }
     }
 
+    /**
+     * Tests that a single client can sequentially send messages, close, open a new connection
+     */
     @Test
     public void test_msg_disconnect_and_reconnect_client() {
         Client client = new Client();
