c1adf07bafb017acc59ab1e84c6d417bca055404
Allen Jue <mrallenjue@yahoo.com>
2022-12-16 20:04:22 -0600

    Change error catch for servern starter

--- src/main/java/ServerStarter.java
+++ src/main/java/ServerStarter.java
@@ -4,8 +4,12 @@ import java.net.ServerSocket;
 public class ServerStarter {
     private static final int DEFAULT_PORT = 3000;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         MultiServer server = new MultiServer();
-        server.start(DEFAULT_PORT);
+        try {
+            server.start(DEFAULT_PORT);
+        } catch (Exception e) {
+            System.out.println("Failed to start server");
+        }
     }
 }
